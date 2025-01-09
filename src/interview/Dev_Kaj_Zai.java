e6142c2e7d3cf060efc5979d845807a49e464e1b
Gupta <ragini-munnaprasad.gupta@capgemini.com>
2025-01-09 17:19:26 +0530

    changes done

---
+++ src/interview/Dev_Kaj_Zai.java
@@ -0,0 +1,57 @@
+package interview;
+
+public class Dev_Kaj_Zai {
+
+/*
+ * 1) Autowired, Dependency injection how you do that
+
+2) Api for creating employee where emp detail name, department and salary
+
+3) What is the return type of that method of create.
+
+4) And if there is an error, how do you handle it?Now suppose that salary is a required field and I did not pass it. It it was blank, say for some reason.So it it will be giving an error right? So how will you do that?
+
+5) Write custom exception, salary is required to create an employee.
+
+6) Angular and its features
+
+7) How you connect frontend application with backend, show me through coding
+
+8) For this url-http://localhost:8293/api/otp/generateCode
+get mapping, Use Observable to observe that value from backend. print the error 
+ 
+9)<div id =outer-div> </div> this dom elements how you can manipulate.
+
+10) Higher order function, callback function
+
+11) Execution flow of the angular application with example.
+
+12) ngModel, #ngmodel reference
+
+13) Custom validator how you have used for the input fields in angular, implements that for the username
+
+14) Form difference Template vs Reactive
+
+15) Let's suppose say I want to use child routing, OK, unit routing and child routing. how you can implements show me through code
+
+16) How you can used child route module in some other components
+There is some methods are the child routes methods, so how will you going to use that child route methods inside one of the different module or different routing file that is a child routing right?
+
+17)Interceptor, 
+
+18) Security, handling token, validating, roles responsibility according to routing the page, how you can implements its show
+
+19) Route guard and outhguard how you can implements
+
+20) Let's consider whatever the employee name could be there. OK, but a grade. OK, when it comes to grade. Grade 84, OK, automatically the salary should have to reflect.28,000, OK.28,000 if grade is A5. Should have to automatically take salary as 52,000.
+grade  
+g- A4 - > 28K 
+g -A5 -> 52K  
+g- A3 - 20 K
+
+21) update the employee details salary whose name starts with A and 'a'
+ 
+ */
+	
+	
+}
