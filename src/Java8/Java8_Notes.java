81f62a20565806a57779b2b138db01951580bfc1
Ragini9819 <ragini-munnaprasad.gupta@capgemini.com>
2025-12-25 17:49:17 +0530

    Technical architecture added

--- src/Java8/Java8_Notes.java
+++ src/Java8/Java8_Notes.java
@@ -1,6 +1,7 @@
 package Java8;
 
-public class Java8_Notes {
+public class Java8_Notes
+{
 /**
  * Java introduced on 18th March 2014
  * Current Version-24 till July 2025
@@ -14,12 +15,26 @@ public class Java8_Notes {
  * 2. Function interface
  * 3. Default and static method
  * 4. Predefined Functional Interface
- * (Predicate, Function, Supplier, Consumer)
+ * ---->(Predicate, Function, Supplier, Consumer)
  * 5. Double colon operator (::)
  * 6. Method reference
  * 7. Streams
  * 8. Date and Time api
  * 9. Optional class
  * 10. Nashorn Javascript engine
+ * 
+ * -------------------------------------------------------------
+ * Old Functional Interface
+ * Runnable- run()
+ * Callable- call()
+ * Comparator- compare()
+ * Comparable- compareTo()
  */
+	
+/*
+ * ********Notes**************
+ * Lambda- The local variable declared or referenced by lambda expression are effectively declared as final.
+ * Whether you declare or not by default its final. 
+ */
+
 }
